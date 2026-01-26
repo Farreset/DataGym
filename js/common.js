b98667d01dc1a2d50b43bed44daa36c028751eae
Farreset <73121885+Farreset@users.noreply.github.com>
2026-01-26 14:05:36 +0100

    Update navigation links to use relative paths

--- js/common.js
+++ js/common.js
@@ -22,23 +22,23 @@ function injectSidebar() {
         </div>
 
         <div class="nav flex-column">
-             <a href="../../" class="nav-link ${(path === '/' || path.includes('index.html')) ? 'active' : ''}">
+             <a href="../" class="nav-link ${(path === '/' || path.includes('index.html')) ? 'active' : ''}">
                 <ion-icon name="home-outline"></ion-icon>
                 <span data-i18n="nav_dashboard">Dashboard</span>
             </a>
-            <a href="../html/swimming" class="nav-link ${path.includes('swimming') ? 'active' : ''}">
+            <a href="html/swimming" class="nav-link ${path.includes('swimming') ? 'active' : ''}">
                 <ion-icon name="water-outline"></ion-icon>
                 <span data-i18n="nav_swimming">Swimming</span>
             </a>
-            <a href="../html/running" class="nav-link ${path.includes('running') ? 'active' : ''}">
+            <a href="html/running" class="nav-link ${path.includes('running') ? 'active' : ''}">
                 <ion-icon name="walk-outline"></ion-icon>
                 <span data-i18n="nav_running">Running</span>
             </a>
-            <a href="../html/plyometrics" class="nav-link ${path.includes('plyometrics') ? 'active' : ''}">
+            <a href="html/plyometrics" class="nav-link ${path.includes('plyometrics') ? 'active' : ''}">
                 <ion-icon name="fitness-outline"></ion-icon>
                 <span data-i18n="nav_plyometrics">Plyometrics</span>
             </a>
-             <a href="../html/gym" class="nav-link ${path.includes('gym') ? 'active' : ''}">
+             <a href="html/gym" class="nav-link ${path.includes('gym') ? 'active' : ''}">
                 <ion-icon name="barbell-outline"></ion-icon>
                 <span data-i18n="nav_gym">Gym</span>
             </a>
